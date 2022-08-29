ab9aabc011d0c89ad63b753ffb80642afb3e1a7a
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-29 23:20:00 +0100

    setup database schema for food and recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_215458) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_221045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.integer "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "foods_recipes", id: false, force: :cascade do |t|
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.integer "quantity"
+    t.index ["food_id", "recipe_id"], name: "index_foods_recipes_on_food_id_and_recipe_id"
+    t.index ["recipe_id", "food_id"], name: "index_foods_recipes_on_recipe_id_and_food_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "prepataion_time"
+    t.string "cooking_time"
+    t.string "description"
+    t.boolean "is_public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -38,4 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_215458) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
