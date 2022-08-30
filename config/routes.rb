1eb68da284e2695eb74f033338aecafcc99392ff
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-30 23:52:15 +0100

    update mroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
    devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        registrations: 'users/registrations'
       } 
       
   root "public_recipes#index"
