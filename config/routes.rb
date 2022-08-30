d763d1feaa4392ed43fe5a3186e8d3b4754ff753
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-08-30 21:14:07 +0100

    customize devise session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root 'user#index'
+  
   root "public_recipes#index"
   resources :recipes, except: [ :update ]
   resources :foods, except: [ :show, :update]
 
- 
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      } 
 
   get "general_shopping_list", to: "general_shopping_list#index"
   get "public_recipes", to: "public_recipes#index"
