abe67a23ddc8b84b183079261ce9837dcdcd80f4
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-09-01 23:38:45 +0100

    add user recipes to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,13 @@
 class RecipesController < ApplicationController
-    def create
-    end
-  
-    def new
-    end
-  
-    def index
-    end
-  
-    def show
-    end
-  
-    def destroy
-    end
-  
-    def public_recipes
-    end
+  def create; end
+
+  def new; end
+
+  def index
+    @recipes = Recipe.where(user_id: current_user.id)
+  end
+
+  def show; end
+
+  def destroy; end
 end
