41326e96f821ea3f775a5b2d3fe728c9a5ffcb20
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-01 17:24:44 -0600

    Add actions in shopping list controller

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,2 +1,9 @@
 class GeneralShoppingListController < ApplicationController
+
+
+  def index
+    @missing_food = RecipeFood.all
+    @amount = RecipeFood.all.count
+    # retrieve_missing_items
+  end
 end
