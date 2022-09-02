9a821698bc16863dd2b4fca0f48db5d33819adf1
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-02 14:15:08 -0600

    Add rspec file for recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :is_public, presence: true, inclusion: [true, false]
 end
