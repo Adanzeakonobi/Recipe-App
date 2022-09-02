1ecf5a9b7d8bf6676778ad6aa2459d6027e118b1
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-02 09:04:50 -0600

    Add spec for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -3,50 +3,48 @@ require 'rails_helper'
 describe Food, type: :model do
   before(:each) do
     @user = User.create!(first_name: 'Adaobi', last_name: 'Okoye', email: 'ada@ada.com', password: 'password')
-    @soya_beans = @user.foods.create(name: 'Soya Beans', price: 3.5, measurement_unit: 'kg', quantity: 10)
+    @food = @user.foods.create(name: 'Akara', price: 200, measurement_unit: 'kg', quantity: 10)
   end
 
   it 'Food should be valid' do
-    expect(@soya_beans).to be_valid
+    expect(@food).to be_valid
   end
 
   it 'Food should not be valid without a name' do
-    @soya_beans.name = nil
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.name = ''
-    expect(@soya_beans).to_not be_valid
+    @food.name = nil
+    expect(@food).to_not be_valid
+    @food.name = ''
+    expect(@food).to_not be_valid
   end
 
   it 'Food should not be valid if name is too long' do
-    @soya_beans.name = 'g' * 251
-    expect(@soya_beans).to_not be_valid
+    @food.name = 'g' * 251
+    expect(@food).to_not be_valid
   end
 
   it 'Food should not be valid without a measurement unit' do
-    @soya_beans.measurement_unit = nil
-    expect(@soya_beans).to_not be_valid
-    @user.name = ''
-    expect(@user).to_not be_valid
+    @food.measurement_unit = nil
+    expect(@food).to_not be_valid
   end
 
   it 'Food should not be valid if wrong measurement unit is passed' do
-    @soya_beans.measurement_unit = 15
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.name = true
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.name = 'gl'
-    expect(@soya_beans).to_not be_valid
+    @food.measurement_unit = 15
+    expect(@food).to_not be_valid
+    @food.name = true
+    expect(@food).to_not be_valid
+    @food.name = 'gl'
+    expect(@food).to_not be_valid
   end
 
   it 'Food should not be valid if price is negative' do
-    @soya_beans.price = - 15
-    expect(@soya_beans).to_not be_valid
+    @food.price = - 15
+    expect(@food).to_not be_valid
   end
 
   it 'Food should not be valid if price is not a number' do
-    @soya_beans.price = 'fifteen'
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.price = true
-    expect(@soya_beans).to_not be_valid
+    @food.price = 'fifteen'
+    expect(@food).to_not be_valid
+    @food.price = true
+    expect(@food).to_not be_valid
   end
 end
