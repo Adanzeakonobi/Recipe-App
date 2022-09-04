5c41e54bef79ab868ad0f5586fa877977a518570
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-09-04 03:18:27 +0100

    move logic away from controller

--- app/helpers/general_shopping_list_helper.rb
+++ app/helpers/general_shopping_list_helper.rb
@@ -1,2 +1,17 @@
 module GeneralShoppingListHelper
+  def generate_data(filtered)
+    data = {}
+    total_cost = 0
+
+    filtered.each do |food, recipefood|
+      difference = food.quantity - recipefood.quantity
+      next unless difference.negative?
+
+      price = -difference * food.price
+      total_cost += price
+      data[food.name] = { difference: -difference, price: }
+    end
+
+    [data, total_cost]
+  end
 end
